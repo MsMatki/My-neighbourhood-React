b35b1b156d1dc4651322d53904c28ab92ce6a38b
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-07-16 22:30:45 -0400

    added more components

--- src/App.js
+++ src/App.js
@@ -2,68 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import escapeRegExp from 'escape-string-regexp';
-import Container from './mapContainer'
-import sortBy from 'sort-by'
-import {Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react'
+import Map from './mapContainer'
+import {GoogleApiWrapper} from 'google-maps-react'
 
 class App extends Component {
 
 state = {
-  showingInfoWindow: false,
-  activeMarker: {},
-  selectedPlace: {},
-  active: true,
-
   sidebar:'open',
-  query: '',
-  places: [
-    {
-        name: 'Westmount Golf & Country Club',
-        position: {lat: 43.4485, lng:  -80.5339},
-    },
-    {
-        name: 'David Johnston Research & Technology Park',
-        position: { lat: 43.4804, lng: -80.5494},
-    },
-    {
-        name: 'Wilfrid Laurier University',
-        position: { lat: 43.4724, lng: -80.5263},
-    },
-    {
-        name: 'University of Waterloo',
-        position: {lat: 43.4723, lng: -80.5449},
-    },
-    {
-        name: 'St. Mary’s General Hospital',
-        position: {lat: 43.4379, lng: -80.5014},
-    }
-],
 }
-
-onMarkerClick = (props, marker, e) => {
-this.setState({
-  selectedPlace: props,
-  activeMarker: marker,
-  showingInfoWindow: true
-});
-}
-
-onMapClicked = (props) => {
-if (this.state.showingInfoWindow) {
-  this.setState({
-    showingInfoWindow: false,
-    activeMarker: null
-  })
-}
-};
-
-updateQuery = (query) => {
-  this.setState({
-    query: query
-  })
-}
-
 
 openCloseMenu = () => {
   let ul = document.querySelector('.side-nav')
@@ -75,23 +21,8 @@ openCloseMenu = () => {
       this.setState({sidebar: 'open'})
     }
 }
-
-
   render() {
-    const {query} = this.state;
-    const {places} = this.state;
-    //get the filter query to filter the listitems 
-    let filteredLocations
-    if (query){
-      const match = new RegExp(escapeRegExp(query),'i')
-      filteredLocations = places.filter((place)=> match.test(place.name))
-    }
-    else{
-      filteredLocations=places
-    }
-    filteredLocations.sort(sortBy('name'))
-
-    
+  
     return (
       <div className="App">
         <header className="header">
@@ -106,51 +37,8 @@ openCloseMenu = () => {
     </ul>
   </div>
   </header>
+  <Map google={this.props.google}/>
 
-  <div className="side-nav">
-  <h2>Popular Places</h2>
-  <div className="searchbox">
-      <input className="search" data-bind="textInput: searchOption, valueUpdate: 'afterkeydown'" placeholder="Search Locations..." 
-      value={this.state.query}
-      onChange={(event) => this.updateQuery(event.target.value)}
-      />
-      </div>
-      <ul className="places">
-      {filteredLocations.map((place) => (     
-        <li key={place.name} className="" onClick={this.onMarkerClick}>{place.name}</li>
-        ))}
-    </ul>
-  </div>
-  <div className="map-container">
-
-  <Map google={this.props.google}
-            onClick={this.onMapClicked}
-            initialCenter={{
-              lat:43.4643, 
-              lng:-80.5204
-            }}
-            zoom={12}
-            >
-            {filteredLocations.map((place) => (
-            <Marker
-            key={place.name}
-            onClick={this.onMarkerClick}
-            position={place.position}
-            name={place.name}
-            ></Marker>
-          ))}
-
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
-          
-            <div>
-              <h3>{this.state.selectedPlace.name}</h3>
-            </div>
-        </InfoWindow>
-      </Map>
-
-      </div>
       </div>
     );
   }
