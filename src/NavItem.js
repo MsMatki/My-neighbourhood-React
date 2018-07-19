ae11605818f0d06ea705e8740bbd632c74a19235
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-07-18 22:10:41 -0400

    added jquery when clicked add active class

--- src/NavItem.js
+++ src/NavItem.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import $ from 'jquery'
+
 
 class Item extends Component{
     openMarker = () => {
-        const {tips, map, infoWindow, marker} = this.props;
+        const {map, infoWindow, marker} = this.props;
         map.panTo(marker.getPosition());
         infoWindow.setContent(
             `<div name=${ marker.title }>
                 <h3>${marker.title}</h3>
             </div>`
         );
-      
+        marker.setAnimation(window.google.maps.Animation.BOUNCE);
+        setTimeout(() => {
+            marker.setAnimation(null)
+        }, 800)
         infoWindow.open(map, marker)
-
     }
+    componentDidMount(){
+        const {marker} = this.props;
+                $('.list li').click(function () {
+                    $('.list .active').removeClass('active');
+                    $(this).addClass('active');
+                  })
+            marker.addListener("click", function() {
+                $('.list .active').removeClass('active');
+            });
+        }   
 
     render(){
         const {marker} = this.props;
         return(
-            <div>
-            <li className="" onClick={this.openMarker}>{marker.title}</li>
+            <div className="list">
+            <li className="card" onClick={this.openMarker}><span className="row"><span className="home"><FontAwesomeIcon icon={faHome} /></span>{marker.title}</span><FontAwesomeIcon icon={faAngleRight} /></li>
             </div>
         )
     }
