07cb8b1e996c8490a526e753bdc4d13d4b2b7ea5
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-07-24 18:10:19 -0400

    added map bounds

--- src/mapContainer.js
+++ src/mapContainer.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import Sidebar from './Sidebar'
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by'
-
+import { Locations } from './Places.js'
 
 class Map extends Component{
-
 
 state = {
     map: {},
@@ -13,48 +12,7 @@ state = {
     markers: [],
     query:'',
     tips:[],
-    places: [
-        {
-            name: 'Cafe O Roaster',
-            position: {lat: 43.4637, lng:  -80.4650},
-            venue_id: '4c69d255d0bdc9b69e0da70b',
-        },
-        {
-            name: 'Aroma Cafe',
-            position: { lat: 43.4647, lng: -80.5235},
-            venue_id: '4b6047c3f964a5207bdd29e3',
-        },
-        {
-            name: 'Princess Cafe',
-            position: { lat: 43.4664, lng: -80.5226},
-            venue_id: '4c03e06139d476b061a630a7',
-        },
-        {
-            name: "Balzac's Coffee",
-            position: {lat: 43.4514, lng: -80.4982},
-            venue_id: '4c814190d34ca14300a92080',
-        },
-        {
-            name: 'City Cafe Bakery',
-            position: {lat: 43.4762, lng: -80.4847},
-            venue_id: '51af397f498eed68816c2557',
-        },
-        {
-            name: 'Williams Coffee Pub',
-            position: {lat: 43.4767, lng: -80.5230},
-            venue_id: '4b9bbfc8f964a520d12036e3',
-        },
-        {
-            name: 'Williams Fresh Café',
-            position: {lat: 43.4724, lng: -80.5385},
-            venue_id: '4b5fa4a8f964a5200bc629e3',
-        },
-        {
-            name: 'Berlin Bicycle Cafe',
-            position: {lat: 43.4536, lng: -80.5182},
-            venue_id: '571e509e498e65ecae235c30',
-        },
-    ],
+    places: Locations
 }
 
     componentDidMount(){
@@ -79,6 +37,16 @@ state = {
             })
         })
     }
+
+    componentDidUpdate(){
+        const {markers,map} = this.state
+        let bounds = new window.google.maps.LatLngBounds();
+
+        markers.forEach((m)=>
+        bounds.extend(m.position))
+        map.fitBounds(bounds)
+    }
+
     loadMap() {
         //Check if Google props has data and Map is loaded --Based on feedback
         if(this.props && this.props.google) {
@@ -91,6 +59,9 @@ state = {
                 content: 'content'
             });
             this.setState({map, infoWindow});
+        }else{
+            //log out Error
+            console.log("Error:Cann't Load Google Map!");
         }
     }
 
@@ -115,14 +86,18 @@ state = {
             position: {lat: place.position.lat, lng: place.position.lng},
             map,
             text: place.text,
-            title: place.name
+            title: place.name,
+            animation: window.google.maps.Animation.DROP,
         });
         marker.addListener('click', () => {
             this.state.map.panTo(marker.getPosition());
             this.state.infoWindow.setContent(`
+            <div class="infoWindow">
                 <div name=${marker.title}>
                     <h3>${marker.title}</h3>
                     <p>${marker.text}</p>  
+                    <p>Tips provided by <a href="https://foursquare.com/">Foursquare</a></p>
+                </div>
                 </div>`);
             this.state.infoWindow.open(map, marker)
         });
