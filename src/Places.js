db73cfd96b1a8cc41b6a524c0b0b3a9108aaa550
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-07-24 18:05:56 -0400

    added new component

---
+++ src/Places.js
@@ -0,0 +1,42 @@
+export const Locations = [
+    {
+        name: 'Cafe O Roaster',
+        position: {lat: 43.4637, lng:  -80.4650},
+        venue_id: '4c69d255d0bdc9b69e0da70b',
+    },
+    {
+        name: 'Aroma Cafe',
+        position: { lat: 43.4647, lng: -80.5235},
+        venue_id: '4b6047c3f964a5207bdd29e3',
+    },
+    {
+        name: 'Princess Cafe',
+        position: { lat: 43.4664, lng: -80.5226},
+        venue_id: '4c03e06139d476b061a630a7',
+    },
+    {
+        name: "Balzac's Coffee",
+        position: {lat: 43.4514, lng: -80.4982},
+        venue_id: '4c814190d34ca14300a92080',
+    },
+    {
+        name: 'City Cafe Bakery',
+        position: {lat: 43.4762, lng: -80.4847},
+        venue_id: '51af397f498eed68816c2557',
+    },
+    {
+        name: 'Williams Coffee Pub',
+        position: {lat: 43.4767, lng: -80.5230},
+        venue_id: '4b9bbfc8f964a520d12036e3',
+    },
+    {
+        name: 'Williams Fresh Café',
+        position: {lat: 43.4724, lng: -80.5385},
+        venue_id: '4b5fa4a8f964a5200bc629e3',
+    },
+    {
+        name: 'Berlin Bicycle Cafe',
+        position: {lat: 43.4536, lng: -80.5182},
+        venue_id: '571e509e498e65ecae235c30',
+    },
+];
